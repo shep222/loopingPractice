95d12f40fd1793709bdcbc77590cc15f520f5dab
John E Shepler Jr <jshepler@suitedconnector.com>
2018-02-02 15:01:59 -0700

    first commit

---
+++ data1.js
@@ -0,0 +1,23 @@
+var movies = [{
+  title: "TopGun",
+  year: 1984,
+  cast: [{
+    firstName: "Tom",
+    lastName: "Cruise"
+  },{
+    firstName: "Kelly",
+    lastName: "Mcginnis"
+  }],
+  producer: "Bob"
+},{
+  title: "Rounders",
+  year: 1997,
+  cast: [{
+    firstName: "Matt",
+    lastName: "Damon"
+  },{
+    firstName: "Edward",
+    lastName: "Norton"
+  }],
+  producer: "Harry"
+}]
